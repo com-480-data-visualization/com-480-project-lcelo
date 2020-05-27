43aa21a1f4c94b3b6386d7e46349500f83a9e252
AAA97AAA <aslam.cader@protonmail.com>
2020-05-27 20:09:45 +0200

    Remove useless lines

--- website/lib/marimekko.js
+++ website/lib/marimekko.js
@@ -252,8 +252,6 @@ class MarimekkoPlot {
 
       function change_color(button, color = "#39A9DB"){
         console.log(button);
-        console.log('ANDIAMO');
-        console.log(button.style('background-color'));
         // rgb(57, 169, 219) === #39A9DB
         if (button.style("background-color") != "rgb(57, 169, 219)"){
           button.style("background-color", "#39A9DB");
